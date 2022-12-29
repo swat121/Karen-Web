0f87c6cc9c74a8df3c69c7f788ca406f64f08203
swat121 <stark2542@gmail.com>
2022-12-29 18:07:08 +0200

    Fix: discovery service
    add anotation @LoadBalanced and @EnableEurekaClient

--- src/main/java/com/karen/KarenWeb/KarenWebApplication.java
+++ src/main/java/com/karen/KarenWeb/KarenWebApplication.java
@@ -4,7 +4,9 @@ package com.karen.KarenWeb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class KarenWebApplication {
 
--- src/main/java/com/karen/KarenWeb/config/RestTemplateConfig.java
+++ src/main/java/com/karen/KarenWeb/config/RestTemplateConfig.java
@@ -1,6 +1,7 @@
 package com.karen.KarenWeb.config;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -11,6 +12,7 @@ import java.time.Duration;
 @Configuration
 public class RestTemplateConfig {
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplateBuilder().setConnectTimeout(Duration.ofSeconds(5)).build();
     }
